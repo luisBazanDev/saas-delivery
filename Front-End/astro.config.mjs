554dcf9ebbad76e417352c54f21fdf613a35f3a5
Luis Bazan <me@luisb.space>
2026-06-18 13:34:03 -0500

    feat: enhance project configuration with server adapter, add user management page, and update styles

--- Front-End/astro.config.mjs
+++ Front-End/astro.config.mjs
@@ -1,14 +1,25 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import node from '@astrojs/node';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
   integrations: [react()],
 
   vite: {
     plugins: [tailwindcss()],
+    optimizeDeps: {
+      include: ['react', 'react-dom', 'react/jsx-dev-runtime'],
+    },
+  },
+  build: {
+    inlineStylesheets: 'always',
   },
 });
